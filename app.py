31f0a400d07461823f8933b03c449ef522cca6e9
sotsuna <mkist202@gmail.com>
2023-09-27 12:16:19 -0300

    nao lembro o que caralhos eu fiz aqui

--- app.py
+++ app.py
@@ -11,55 +11,70 @@ app.secret_key = 'segredo'
 # array com as respostas corretas
 respostas_corretas = {
     1: [
+        {'question_level': 'First Question'},
+        {'questao': 'What is the correct conjugation of the verb "wish" in the "past perfect"?'},
         {'opcao': 'had wished', 'correta': True},
         {'opcao': 'wishing', 'correta': False},
         {'opcao': 'wish', 'correta': False},
         {'opcao': 'wished', 'correta': False},
     ],
     2: [
+        {'question_level': 'Second Question'},
+        {'questao': 'What is the correct conjugation of the verb "wish" in the "past perfect"?'},
         {'opcao': 'wish', 'correta': True},
         {'opcao': 'wished', 'correta': False},
         {'opcao': 'wishing', 'correta': False},
         {'opcao': 'had wished', 'correta': False},
     ],
     3: [
+        {'question_level': 'Third Question'},
+        {'questao': 'What is the correct conjugation of the verb "wish" in the "past perfect"?'},
         {'opcao': 'had arrived', 'correta': True},
         {'opcao': 'arrives', 'correta': False},
         {'opcao': 'arrived', 'correta': False},
         {'opcao': 'arrive', 'correta': False},
     ],
     4: [
+        {'question_level': 'Fourth Question'},
+        {'questao': 'What is the correct conjugation of the verb "wish" in the "past perfect"?'},
         {'opcao': 'wished', 'correta': True},
         {'opcao': 'had wish', 'correta': False},
         {'opcao': 'wish', 'correta': False},
         {'opcao': 'had wished', 'correta': False},
     ],
     5: [
+        {'question_level': 'Fifth Question'},
+        {'questao': 'What is the correct conjugation of the verb "wish" in the "past perfect"?'},
         {'opcao': 'had left', 'correta': True},
         {'opcao': 'leaves', 'correta': False},
         {'opcao': 'left', 'correta': False},
         {'opcao': 'leave', 'correta': False},
     ],
     6: [
+        {'question_level': 'Sixth Question'},
+        {'questao': 'What is the correct conjugation of the verb "wish" in the "past perfect"?'},
         {'opcao': 'had arrived', 'correta': True},
         {'opcao': 'arrives', 'correta': False},
         {'opcao': 'arrived', 'correta': False},
         {'opcao': 'arrive', 'correta': False},
     ],
     7: [
+        {'question_level': 'Seventh Question'},
+        {'questao': 'What is the correct conjugation of the verb "wish" in the "past perfect"?'},
         {'opcao': 'had arrived', 'correta': True},
         {'opcao': 'arrives', 'correta': False},
         {'opcao': 'arrived', 'correta': False},
         {'opcao': 'arrive', 'correta': False},
     ],
     8: [
+        {'question_level': 'Eighth Question'},
+        {'questao': 'What is the correct conjugation of the verb "wish" in the "past perfect"?'},
         {'opcao': 'had arrived', 'correta': True},
         {'opcao': 'arrives', 'correta': False},
         {'opcao': 'arrived', 'correta': False},
         {'opcao': 'arrive', 'correta': False},
     ],
 }
-
 
 # route para puxar a index
 @app.route('/')
@@ -69,13 +84,12 @@ def index():
 # route para puxar a tela de introdução do jogo
 @app.route('/introduction', methods=['GET', 'POST'])
 def namePage():
+    name = None  # Inicialize a variável name como None
     if request.method == 'POST':
         name = request.form['name']
-        return render_template('levels/1.html', name=name)
-    else:
-        return render_template('introduction.html')
+    return render_template('introduction.html', name=name)
 
-# route para puxar o sistema de níveis e testar se a resposta concedida foi a correta
+# Route para puxar o sistema de níveis e testar se a resposta concedida foi a correta
 @app.route('/level', methods=['GET', 'POST'])
 def levelPage():
     # Verifique se 'levelnum' está na sessão, se não, defina-o como 1
@@ -85,40 +99,45 @@ def levelPage():
     # Obtém o número do nível atual
     levelnum = session['levelnum']
 
+    # Obtém as opções de resposta para o nível atual
+    respostas_nivel_atual = respostas_corretas.get(levelnum, [])
+
+    # Conta quantos botões estão visíveis
+    quantidade_botao_visivel = len([opcao for opcao in respostas_nivel_atual if not opcao.get('oculto')])
+
+    # Calcula a probabilidade com base na quantidade de botões visíveis
+    probabilidade_acerto = 100 / quantidade_botao_visivel if quantidade_botao_visivel > 0 else 0
+
     mensagem_erro = None  # Inicialize a variável de mensagem de erro como None
 
     if request.method == 'POST':
         resposta_usuario = request.form.get('resposta')
-        respostas_nivel_atual = respostas_corretas.get(levelnum)
 
-        # Verifique se a resposta do usuário está entre as opções corretas
-        resposta_correta = any(opcao['opcao'].lower() == resposta_usuario.lower() and opcao['correta'] for opcao in respostas_nivel_atual)
+        # Verifica se a resposta do usuário está entre as opções corretas
+        resposta_correta = any(opcao.get('correta', False) and opcao.get('opcao', '').lower() == resposta_usuario.lower() for opcao in respostas_nivel_atual)
 
         if not resposta_correta:
             # Se a resposta estiver incorreta, marque o botão clicado como oculto
             botao_errado = request.form.get('resposta')
-            mensagem_erro = "Wrong answer! I removed the wrong option for you."
+            mensagem_erro = "Resposta incorreta. Tente novamente."
             for opcao in respostas_nivel_atual:
-                if opcao['opcao'] == botao_errado:
+                if opcao.get('opcao', '').lower() == resposta_usuario.lower():
                     opcao['oculto'] = True
 
         # Se a resposta estiver correta, vá para o próximo nível
         if resposta_correta:
             session['levelnum'] += 1
-            levelnum = session['levelnum']
 
-    # Obtenha as opções de resposta para o nível atual
-    opcoes_resposta = respostas_corretas[levelnum]
+    # Filtra as opções de resposta visíveis (não ocultas)
+    opcoes_resposta_visiveis = [opcao for opcao in respostas_nivel_atual if not opcao.get('oculto')]
 
-    # Renderize o template HTML e passe as opções de resposta e a mensagem de erro
-    return render_template('levels/{}.html'.format(levelnum), opcoes_resposta=opcoes_resposta, mensagem_erro=mensagem_erro)
-
+    # Renderize o template HTML e passe as opções de resposta, a mensagem de erro e a probabilidade de acerto
+    return render_template('levels/{}.html'.format(levelnum), opcoes_resposta=opcoes_resposta_visiveis, mensagem_erro=mensagem_erro, probabilidade_acerto=probabilidade_acerto, name=name)
 
 
 @app.route('/about', methods=['GET', 'POST'])
 def aboutPage():
     return render_template('about.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
